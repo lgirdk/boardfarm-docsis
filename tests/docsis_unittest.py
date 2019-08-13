cf6b2bcbbd2c105eb1a817b9c9b6ae75c9e23433
rshanigarapu-contractor <rshanigarapu.contractor@libertyglobal.com>
2019-08-13 19:06:00 +0200

    tests: added class to unit test cmts methods
    added class and methods to unit test the methods in devices/<cmts>
    
    Signed-off-by: rshanigarapu-contractor <rshanigarapu.contractor@libertyglobal.com>
    Change-Id: I8767a9b666d81075b8e58aec32fb8810161498f1
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm-docsis/+/92
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Matthew McClintock <matthew@meldnetworks.com>

---
+++ tests/docsis_unittest.py
@@ -0,0 +1,19 @@
+import rootfs_boot
+from devices import board
+class selftest_test_cmts_functions(rootfs_boot.RootFSBootTest):
+    def runTest(self):
+            '''
+            This function is to test the cmts functions moved to boardfarm-docsis/devices/base_cmts.py.
+            Input : None (self -- cmts object).
+            Output : None (checks the cmts methods and prints the output).
+            '''
+            from devices import cmts
+            ip_provisioning_mode=cmts.check_docsis_mac_ip_provisioning_mode(cmts.mac_domain)
+            print ("The ip provisioning mode on given mac domain is %s" % ip_provisioning_mode)
+            cmts.wait_for_ready()
+            is_cm_bridged=cmts.is_cm_bridged(board.config["cm_mac"])
+            print ("The status of cm bridged is %s" % is_cm_bridged)
+            ertr_ipv4=cmts.get_ertr_ipv4(board.config["cm_mac"])
+            print ("The erouter Ipv4 address is %s" % ertr_ipv4)
+            ertr_ipv6=cmts.get_ertr_ipv6(board.config["cm_mac"])
+            print ("The erouter Ipv6 address is %s" % ertr_ipv6)
