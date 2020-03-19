e44aca415a9576a9acfaab7c93e7191fdf050c06
Mike Anderson <mike@meldnetworks.com>
2020-03-19 23:28:27 +0000

    Run yapf to autoformat remaining files
    Signed-off-by: Mike Anderson <mike@meldnetworks.com>
    Change-Id: Ia89e92a055eb54b82c3557654a79e1a705b69d4c
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm-docsis/+/4063
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Prem Kumar K N <prekumar.contractor@libertyglobal.com>

--- boardfarm_docsis/exceptions.py
+++ boardfarm_docsis/exceptions.py
@@ -1,22 +1,29 @@
 from boardfarm.exceptions import BftBaseException
 
+
 class BftDocsisBaseException(BftBaseException):
     '''Base Docsis exception'''
+
 
 class CfgEncodeFailed(BftBaseException):
     '''Encoding of Cfg file failed'''
 
+
 class CMCfgEncodeFailed(CfgEncodeFailed):
     '''Exception that occurs when MTA encoding fails'''
+
 
 class MTACfgEncodeFailed(CfgEncodeFailed):
     '''Exception that occurs when MTA encoding fails'''
 
+
 class CfgUnknownType(CfgEncodeFailed):
     '''Unknown type of cfg'''
 
+
 class VoiceSetupConfigureFailure(BftBaseException):
     '''Exception that occurs when Voice setup is not configured'''
+
 
 class BftProvEnvMismatch(BftBaseException):
     '''Exception that occurs when prov mode is not equal to environment'''
