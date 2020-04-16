40cf7a576c8550f06db2863797cfaa70eedba75b
ketan.d.luffy <ktewari.contractor@libertyglobal.com>
2020-04-16 13:39:15 +0200

    lib: booting: skip TR-069 checks for disabled mode
    Signed-off-by: ketan.d.luffy <ktewari.contractor@libertyglobal.com>
    Change-Id: Ie132233716f99c610f40208a3dc5a691102b55d1
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm-docsis/+/4621
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- boardfarm_docsis/lib/booting.py
+++ boardfarm_docsis/lib/booting.py
@@ -9,6 +9,7 @@ def boot(config, env_helper, devices, logged=dict()):
     ertr_mode = env_helper.get_ertr_mode()
     country = env_helper.get_country()
     voice = env_helper.voice_enabled()
+    tr069check = cfg not in ["disabled", "bridge"]
 
     logged['boot_step'] = "env_ok"
 
@@ -46,10 +47,11 @@ def boot(config, env_helper, devices, logged=dict()):
             devices.board.wait_for_mta_provisioning()
             logged['boot_step'] = "voice_mta_ok"
 
-        devices.board.restart_tr069(
-            devices.wan,
-            devices.board.get_interface_ipaddr(devices.board.wan_iface))
-        logged['boot_step'] = "acs_connection_ok"
+        if tr069check:
+            devices.board.restart_tr069(
+                devices.wan,
+                devices.board.get_interface_ipaddr(devices.board.wan_iface))
+            logged['boot_step'] = "acs_connection_ok"
     except NoTFTPServer as e:
         raise e
     except Exception as e:
