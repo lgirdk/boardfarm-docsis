b373aa92a86fcca3946eb9a4e218f75f89715288
Nagaraj MS <nnagaraj.contractor@libertyglobal.com>
2020-04-16 13:20:04 +0200

    tests: reset_to_defaults: Fix for MVX_TST-3131
    Signed-off-by: Nagaraj MS <nnagaraj.contractor@libertyglobal.com>
    Change-Id: Iec3a58a1e91a8aa98e376e6ffe7024112810216a
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm-docsis/+/4615
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm_docsis/tests/reset_to_defaults.py
+++ boardfarm_docsis/tests/reset_to_defaults.py
@@ -1,5 +1,4 @@
 from boardfarm.exceptions import BootFail
-from boardfarm.lib.common import retry_on_exception
 from boardfarm_docsis.tests.docsis_boot import DocsisBootStub as BF_Test
 
 
@@ -9,12 +8,11 @@ class reset_board_to_defaults(BF_Test):
     def runTest(self):
         try:
             self.dev.board.reset_defaults_via_os(self)
-            self.eRouter_ip = retry_on_exception(
-                self.dev.board.get_interface_ipaddr,
-                (self.dev.board.erouter_iface, ),
-                retries=5)
-            self.dev.board.restart_tr069(
-                self.dev.wan,
-                self.dev.board.get_interface_ipaddr(self.dev.board.wan_iface))
+            self.dev.board.check_valid_docsis_ip_networking()
+            if self.env_helper.get_prov_mode() not in ["bridge", "disabled"]:
+                self.dev.board.restart_tr069(
+                    self.dev.wan,
+                    self.dev.board.get_interface_ipaddr(
+                        self.dev.board.wan_iface))
         except Exception:
             raise BootFail("BootFail : Failed to Factory Reset")
