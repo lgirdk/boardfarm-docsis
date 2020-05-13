999bbc69554f17fda6b2fe274c10e7ab5e191200
mgualco.contractor <mgualco.contractor@libertyglobal.com>
2020-05-13 17:46:50 +0000

    lib: booting: removed restart_tr069
    it will be be moved to a test of its own
    
    Signed-off-by: mgualco.contractor <mgualco.contractor@libertyglobal.com>
    Change-Id: I99ccd2655db4839eb8a540c3ead05ca1310e1432
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm-docsis/+/5564
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm_docsis/lib/booting.py
+++ boardfarm_docsis/lib/booting.py
@@ -9,7 +9,6 @@ def boot(config, env_helper, devices, logged=dict()):
     ertr_mode = env_helper.get_ertr_mode()
     country = env_helper.get_country()
     voice = env_helper.voice_enabled()
-    tr069check = cfg not in ["disabled", "bridge", "none"]
 
     logged['boot_step'] = "env_ok"
 
@@ -57,11 +56,6 @@ def boot(config, env_helper, devices, logged=dict()):
             devices.board.wait_for_mta_provisioning()
             logged['boot_step'] = "voice_mta_ok"
 
-        if tr069check:
-            devices.board.restart_tr069(
-                devices.wan,
-                devices.board.get_interface_ipaddr(devices.board.wan_iface))
-            logged['boot_step'] = "acs_connection_ok"
     except NoTFTPServer as e:
         raise e
     except Exception as e:
