ec2fc2dc50dfa998c74e3522e5123b5d7305bf6e
ketan.tewari <ktewari.contractor@libertyglobal.com>
2020-10-14 15:32:28 +0200

    devices: placeholder for SW matching logic
    Signed-off-by: ketan.tewari <ktewari.contractor@libertyglobal.com>
    Change-Id: I90e181d3d36359839c54bfb3d6f61389a602666e
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm-docsis/+/8727
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm_docsis/__init__.py
+++ boardfarm_docsis/__init__.py
@@ -1,7 +1,9 @@
 # Copyright (c) 2019
 #
 # All rights reserved.
+from . import devices  # noqa: F401
 
 __version__ = "1.0.0"
 
 selftest_testsuite = "selftest-docsis"
+override_probe_devices = True
