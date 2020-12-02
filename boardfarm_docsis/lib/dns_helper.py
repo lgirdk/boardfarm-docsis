549437b82c0be99adafd10e704bb9273c5c0315c
Kottesvaran Murugan <mkottesvaran.contractor@libertyglobal.com>
2020-12-02 21:46:50 +0100

    lib: dns_helper: split reachable/unreachable ips to ipv4 and ipv6 reachable/unreachable
    Depends-on: I805fba2247248de9b02a0257a9b260d89ccc74b1
    
    Signed-off-by: Kottesvaran Murugan <mkottesvaran.contractor@libertyglobal.com>
    Change-Id: I804733c3d4cf8f18328313c245df9b298f5f35c3
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm-docsis/+/9670
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm_docsis/lib/dns_helper.py
+++ boardfarm_docsis/lib/dns_helper.py
@@ -3,6 +3,8 @@ def dns_acs_config(devices, dns_dict):
         for dev, host_data in dns_dict.items():
             d = getattr(devices, dev.lower())
             d.dns.configure_hosts(
-                reachable_ips=host_data["num_reachable_ip"],
-                unreachable_ips=host_data["num_unreachable_ip"],
+                reachable_ipv4=host_data["ipv4"]["reachable"],
+                unreachable_ipv4=host_data["ipv4"]["unreachable"],
+                reachable_ipv6=host_data["ipv6"]["reachable"],
+                unreachable_ipv6=host_data["ipv6"]["unreachable"],
             )
