803373acc3cbbfa4d89e86e61b72bb29b8ca954c
Michele Gualco <mgualco.contractor@libertyglobal.com>
2021-01-19 07:38:32 +0100

    integrationtests: erouter mode "none" tests
    Signed-off-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Change-Id: I8cfe78faed72197841d4915e446a7f3b7fc88e84
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm-docsis/+/10232
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- integrationtests/boardfarm_docsis/lib/test_env_helper.py
+++ integrationtests/boardfarm_docsis/lib/test_env_helper.py
@@ -217,7 +217,238 @@ class TestEnvHelper:
             },
             True,
         ],
+        [
+            {
+                "environment_def": {
+                    "board": {
+                        "eRouter_Provisioning_mode": "none",
+                        "config_boot": {
+                            "vendor_specific": {
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    "TlvCode 12 TlvString 'Device.X_LGI-COM_General.CustomerId|unsignedInt|20'",
+                                ],
+                            }
+                        },
+                    },
+                },
+                "version": "2.3",
+            },
+            {
+                "environment_def": {
+                    "board": {
+                        "eRouter_Provisioning_mode": [
+                            "ipv6",
+                            "dual",
+                            "ipv4",
+                        ],  # missing "none"
+                        "config_boot": {
+                            "vendor_specific": {
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    "TlvCode 12 TlvString 'Device.X_LGI-COM_General.CustomerId|unsignedInt|20'",
+                                ],
+                            }
+                        },
+                    }
+                }
+            },
+            True,
+        ],
+        [
+            {
+                "environment_def": {
+                    "board": {
+                        "eRouter_Provisioning_mode": "none",
+                        "config_boot": {
+                            "vendor_specific": {
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    "TlvCode 12 TlvString 'Device.X_LGI-COM_General.CustomerId|unsignedInt|20'",
+                                ],
+                            }
+                        },
+                    },
+                },
+                "version": "2.3",
+            },
+            {
+                "environment_def": {
+                    "board": {
+                        "eRouter_Provisioning_mode": [
+                            "non",
+                            "ipv6",
+                            "dual",
+                            "ipv4",
+                        ],  # misspelled "non"
+                        "config_boot": {
+                            "vendor_specific": {
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    "TlvCode 12 TlvString 'Device.X_LGI-COM_General.CustomerId|unsignedInt|20'",
+                                ],
+                            }
+                        },
+                    }
+                }
+            },
+            True,
+        ],
+        [
+            {
+                "environment_def": {
+                    "board": {
+                        "eRouter_Provisioning_mode": "none",
+                        "config_boot": {
+                            "vendor_specific": {
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    "TlvCode 12 TlvString 'Device.X_LGI-COM_General.CustomerId|unsignedInt|20'",
+                                ],
+                            }
+                        },
+                    },
+                },
+                "version": "2.3",
+            },
+            {
+                "environment_def": {
+                    "board": {
+                        "eRouter_Provisioning_mode": ["none", "ipv6", "dual", "ipv4"],
+                        "config_boot": {
+                            "vendor_specific": {
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    "TlvCode 12 TlvString 'Device.X_LGI-COM_General.CustomerId|unsignedInt|20'",
+                                ],
+                            }
+                        },
+                    }
+                }
+            },
+            False,
+        ],
+        [
+            {
+                "environment_def": {
+                    "board": {
+                        "eRouter_Provisioning_mode": "none",
+                        "config_boot": {
+                            "vendor_specific": {
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    "TlvCode 12 TlvString 'Device.X_LGI-COM_General.CustomerId|unsignedInt|20'",
+                                ],
+                            }
+                        },
+                    },
+                },
+                "version": "2.3",
+            },
+            {
+                "environment_def": {
+                    "board": {
+                        "eRouter_Provisioning_mode": ["none", "ipv6", "dual", "ipv4"],
+                        "config_boot": {
+                            "vendor_specific": {
+                                "tlvs": [
+                                    "InitializationMode 1",  # differs
+                                    "TlvCode 12 TlvString 'Device.X_LGI-COM_General.CustomerId|unsignedInt|20'",
+                                ],
+                            }
+                        },
+                    }
+                }
+            },
+            True,
+        ],
+        [
+            {
+                "environment_def": {
+                    "board": {
+                        "eRouter_Provisioning_mode": "none",
+                        "config_boot": {
+                            "vendor_specific": {
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    "TlvCode 12 TlvString 'Device.X_LGI-COM_General.CustomerId|unsignedInt|20'",
+                                ],
+                            }
+                        },
+                    },
+                },
+                "version": "2.3",
+            },
+            {
+                "environment_def": {
+                    "board": {
+                        "eRouter_Provisioning_mode": ["none", "ipv6", "dual", "ipv4"],
+                        "config_boot": {
+                            "vendor_specific": {
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    "TlvCode 12 TlvString 'Device.X_LGI-COM_General.CustomerId|unsignedInt|20'",
+                                ],
+                            }
+                        },
+                    }
+                }
+            },
+            False,
+        ],
+        [
+            {
+                "environment_def": {
+                    "CMTS": {"model": "C4", "type": "arris"},
+                    "board": {
+                        "config_boot": {
+                            "vendor_specific": {
+                                "component": "eRouter",
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    'TlvCode 12 TlvString "Device.X_LGI-COM_General.CustomerId|unsignedInt|20"',
+                                ],
+                            }
+                        },
+                        "country": "NL",
+                        "eRouter_Provisioning_mode": "none",
+                        "emta": {"config_template": "CH_Compal"},
+                        "model": "CH7465LG",
+                        "software": {
+                            "flash_strategy": "all",
+                            "image_uri": "/OneFirmware/DailyBuilds/Jan_2021/20210112/arm-image-rdkb-mv1intel-20210112072903.img",
+                        },
+                    },
+                    "tr-069": {},
+                    "voice": {},
+                },
+                "version": "2.10",
+            },
+            {
+                "environment_def": {
+                    "board": {
+                        "config_boot": {
+                            "vendor_specific": {
+                                "component": "eRouter",
+                                "tlvs": [
+                                    "InitializationMode 0",
+                                    'TlvCode 12 TlvString "Device.X_LGI-COM_General.CustomerId|unsignedInt|20"',
+                                ],
+                            }
+                        },
+                        "eRouter_Provisioning_mode": "none",
+                    },
+                }
+            },
+            False,
+        ],
     ]
+
+    """
+    environment: env definition (e.g. from Jira)
+    requested: env requested (e.g.iwhat a test wants)
+    raises: whether this test will raise an exception or not
+    """
 
     @pytest.mark.parametrize(
         "environment, requested, raises",
@@ -228,6 +459,12 @@ class TestEnvHelper:
             (environments[3][0], environments[3][1], environments[3][2]),
             (environments[4][0], environments[4][1], environments[4][2]),
             (environments[5][0], environments[5][1], environments[5][2]),
+            (environments[6][0], environments[6][1], environments[6][2]),
+            (environments[7][0], environments[7][1], environments[7][2]),
+            (environments[8][0], environments[8][1], environments[8][2]),
+            (environments[9][0], environments[9][1], environments[9][2]),
+            (environments[10][0], environments[10][1], environments[10][2]),
+            (environments[11][0], environments[11][1], environments[11][2]),
         ],
     )
     def test_env_check(self, environment, requested, raises):
