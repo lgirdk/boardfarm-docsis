2d007d9637d9dabcd6b25f2c5c9ba33c72015132
meghnanagaraj <mnagaraj.contractor@libertyglobal.com>
2021-02-16 00:29:22 +0100

    boardfarm-docsis:lib:hooks:contingency_checks::Added service checks for Default,Voice and Check Interface
    Signed-off-by: meghnanagaraj <mnagaraj.contractor@libertyglobal.com>
    Change-Id: I8f53da05a0e18ae946f9f3afba1d23004f9c8d36
    Depends-on: Iec8f707b88ef99e381d62319f28138f3ea076bd3
    Depends-on: I857c4a97dd5f9cafdbd8323f65b1267fdd213058
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm-docsis/+/10612
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm_docsis/lib/hooks/contingency_checks.py
+++ boardfarm_docsis/lib/hooks/contingency_checks.py
@@ -1,7 +1,23 @@
 """Provide Hook implementations for contingency checks for BF-docsis plugin."""
 
+import json
+import logging
+
+from boardfarm.exceptions import BftSysExit
+from boardfarm.lib.DeviceManager import device_type
 from boardfarm.lib.hooks import contingency_impl, hookimpl
 from boardfarm.plugins import BFPluginManager
+from termcolor import colored
+
+from boardfarm_docsis.lib.docsis import (
+    check_board,
+    check_cm_firmware_version,
+    check_interface,
+    check_provisioning,
+)
+from boardfarm_docsis.lib.voice import check_peer_registration
+
+logger = logging.getLogger("bft")
 
 
 class ContingencyCheck:
@@ -12,7 +28,7 @@ class ContingencyCheck:
     # NOTE: not using tryfirst here, as we want boardfarm contingency check
     # to be executed first
     @hookimpl
-    def contingency_check(self, env_req, dev_mgr):
+    def contingency_check(self, env_req, dev_mgr, env_helper):
         """Register service check plugins based on env_req.
 
         Reading the key value pairs from env_req, BFPluginManager scans
@@ -25,32 +41,136 @@ class ContingencyCheck:
         :param env_req: ENV request provided by a test
         :type env_req: dict
         """
-        # Print statement can be removed later. Kept for understand exec flow
-        print("This is BF docsis contingency check")
+        print(
+            "Executing BF Docsis contingency service check",
+            end=("\n" + "-" * 80 + "\n"),
+        )
 
         pm = BFPluginManager("contingency")
         pm.load_hook_specs("feature")
         all_impls = pm.fetch_impl_classes("feature")
 
-        # same theory here, based on env_req decide which plugin to register
-        # TODO: Add right set of rules here.
+        plugins_to_register = [all_impls["boardfarm_docsis.DefaultChecks"]]
+
         if "voice" in env_req.get("environment_def", {}):
-            pm.register(all_impls["boardfarm_docsis.Voice"])
-        pm.hook.service_check(env_req=env_req, dev_mgr=dev_mgr)
+            plugins_to_register.append(all_impls["boardfarm_docsis.Voice"])
+
+        plugins_to_register.append(all_impls["boardfarm_docsis.CheckInterface"])
+
+        for i in reversed(plugins_to_register):
+            pm.register(i)
+        result = pm.hook.service_check(
+            env_req=env_req, dev_mgr=dev_mgr, env_helper=env_helper
+        )
 
         # this needs to be orchestrated by hook wrapper maybe
         BFPluginManager.remove_plugin_manager("contingency")
+        return result
 
 
-class Voice:
-    """Dummy implementation for Demo.
-
-    Consider this to be an implementation for PacketCable 1.5
-    """
+class DefaultChecks:
+    """Perform these checks even if ENV req is empty."""
 
     impl_type = "feature"
 
     @contingency_impl
-    def service_check(self, env_req, dev_mgr):
+    def service_check(self, env_req, dev_mgr, env_helper):
+        """Implement Default Contingency Hook."""
+        print(
+            "Executing Default service check for BF Docsis",
+            end=("\n" + "-" * 80 + "\n"),
+        )
+
+        if not dev_mgr.board.is_erouter_honouring_config():
+            msg = colored(
+                "esafeErouterInitModeControl not set to 5 (honoreRouterInitMode)!!!!!",
+                color="red",
+                attrs=["bold"],
+            )
+            logger.critical(msg)
+            logger.critical("Possible hint: a previous test did not cleanup?")
+            raise BftSysExit(msg)
+
+        cm_mac = dev_mgr.board.config["cm_mac"]
+        voice = "voice" in env_req.get("environment_def", {})
+        board = dev_mgr.by_type(device_type.DUT)
+        wan = dev_mgr.by_type(device_type.wan)
+        cmts = dev_mgr.by_type(device_type.cmts)
+
+        check_board(board, cmts, cm_mac)
+        check_provisioning(dev_mgr.board, mta=voice)
+        check_cm_firmware_version(board, wan, env_helper)
+
+        print(
+            "Default service checks for BF DOCSIS executed",
+            end=("\n" + "-" * 80 + "\n"),
+        )
+
+
+class CheckInterface:
+    """Perform these checks even if ENV req is empty."""
+
+    impl_type = "feature"
+
+    @contingency_impl(trylast=True)
+    def service_check(self, env_req, dev_mgr, env_helper):
+        """Implement Default Contingency Hook."""
+        print(
+            "Executing service check for BF DOCSIS CheckInterface",
+            end=("\n" + "-" * 80 + "\n"),
+        )
+
+        ip = {}
+        board = dev_mgr.by_type(device_type.DUT)
+
+        if env_helper.has_prov_mode():
+            prov_mode = env_helper.get_prov_mode()
+        else:
+            prov_mode = "dual"  # default prov mode is dual
+
+        ip_ifaces = [
+            board.mta_iface,
+            board.wan_iface,
+            board.erouter_iface,
+        ]
+
+        if prov_mode.lower() in ["dslite", "ipv6"]:
+            ip_ifaces.append(board.aftr_iface)
+        elif prov_mode.lower() in ["disabled", "bridge"]:
+            ip_ifaces.append(board.lan0_iface)
+
+        ip["board"] = board.get_ifaces_ip_dict(ip_ifaces)
+
+        try:
+            # validate if all collected ip addresses are as per provisioning mode.
+            check_interface(board, ip, prov_mode, [])
+        except Exception as e:
+            print("Interface check failed.\nReason: %s" % e)
+            print(json.dumps(ip, indent=4))
+            raise (e)
+
+        print(
+            "Service checks for BF DOCSIS CheckInterface executed",
+            end=("\n" + "-" * 80 + "\n"),
+        )
+
+        return ip
+
+
+class Voice:
+
+    impl_type = "feature"
+
+    @contingency_impl
+    def service_check(self, env_req, dev_mgr, env_helper):
         """Implement contingency check for voice."""
+        print("Executing service check for Voice", end=("\n" + "-" * 80 + "\n"))
+
+        sipserver = dev_mgr.by_type(device_type.sipcenter)
+        board = dev_mgr.by_type(device_type.DUT)
+        lan_devices = [dev_mgr.lan, dev_mgr.lan2]
+        num_list = [lan.own_number for lan in lan_devices]
+
+        check_peer_registration(board, num_list, sipserver)
+
         print("Voice service checks executed", end=("\n" + "-" * 80 + "\n"))
