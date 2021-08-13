59ca3bb1f77a0e1a396b4b1aa441da0d63c4bba4
Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
2021-08-13 15:26:20 +0000

    Flit changes boardfarm_docsis repo
    Signed-off-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Change-Id: Ife713d0095943348a75d234ed064d69ec2068485
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm-docsis/+/12997
    Tested-by: Filippo Carra <fcarra@libertyglobal.com>
    Reviewed-by: Filippo Carra <fcarra@libertyglobal.com>

--- boardfarm_docsis/__init__.py
+++ boardfarm_docsis/__init__.py
@@ -1,9 +1,7 @@
-# Copyright (c) 2019
-#
-# All rights reserved.
-from . import devices  # noqa: F401
-
+"""An add-on to boardfarm that contains DOCSIS specific libraries"""
 __version__ = "1.0.0"
+
+from . import devices  # noqa: F401
 
 selftest_testsuite = "selftest-docsis"
 override_probe_devices = True
