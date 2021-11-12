b95097a11a7e3ee67e5d91e65c94e511e7caca56
Michele Gualco <mgualco.contractor@libertyglobal.com>
2021-11-12 16:11:24 +0100

    fix: board not online on cmts
    Temporary fix for boards that do not come online on CMTS, reboot and try one more time.
    
    Signed-off-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Change-Id: I97842ef63673746d9abaf5bf450b26a8cbe38f15
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm-docsis/+/14026
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>
    (cherry picked from commit 3c1cf3c5ca42dd6ff69d8f15c583fa0e80a0d9d7)
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm-docsis/+/14044

--- boardfarm_docsis/lib/booting.py
+++ boardfarm_docsis/lib/booting.py
@@ -152,8 +152,7 @@ def boot_board(config, env_helper, devices):
 boot_actions = {"board_boot": boot_board}
 
 
-def post_boot_board(config, env_helper, devices):
-
+def _wait_for_cm_online(config, env_helper, devices):
     for _ in range(180):
         if devices.cmts.is_cm_online(ignore_partial=True) is False:
             # show the arm prompt as it is a log in itself
@@ -162,16 +161,32 @@ def post_boot_board(config, env_helper, devices):
             continue
         if devices.board.finalize_boot():
             break
-        else:
-            devices.board.wait_for_reboot(timeout=900)
-            logger.info("######Rebooting######")
-            devices.cmts.clear_cm_reset(devices.board.cm_mac)
-            time.sleep(20)
-
+        devices.board.wait_for_reboot(timeout=900)
+        logger.info("######Rebooting######")
+        devices.cmts.clear_cm_reset(devices.board.cm_mac)
+        time.sleep(20)
     else:
         msg = "\n\nFailed to Boot: board not online on CMTS"
         logger.critical(msg)
         raise BootFail(msg)
+
+
+def post_boot_board(config, env_helper, devices):
+
+    try:
+        _wait_for_cm_online(config, env_helper, devices)
+    except BootFail:
+        logger.warning(
+            colored(
+                "Board not online on CMTS, resetting and retrying",
+                color="yellow",
+                attrs=["bold"],
+            )
+        )
+        devices.board.reset()
+        devices.cmts.clear_cm_reset(devices.board.cm_mac)
+        _wait_for_cm_online(config, env_helper, devices)
+
     devices.board.post_boot_init()
     board_uptime = devices.board.get_seconds_uptime()
     logger.info(f"Time up: {board_uptime}")
