716401fb7ac41a64bfe8733544fdd67330d5f09e
nnagaraj <nnagaraj.contractor@libertyglobal.com>
2021-11-17 11:33:38 +0100

    feat(provision_helper.py): add use cases for verifcation of cfg file download/apply process
    Signed-off-by: nnagaraj <nnagaraj.contractor@libertyglobal.com>
    Change-Id: I619337d6efcf11c55790c24520d8b4d24209766d
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm-docsis/+/14068
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm_docsis/use_cases/provision_helper.py
+++ boardfarm_docsis/use_cases/provision_helper.py
@@ -1,7 +1,8 @@
 import inspect
 from typing import Optional
 
-from boardfarm.lib.DeviceManager import device_manager
+from boardfarm.exceptions import CodeError
+from boardfarm.lib.DeviceManager import device_manager, get_device_by_name
 
 
 def provision_board():
@@ -35,3 +36,92 @@ def provision_board_boot_file(
 
 def _push_to_tftp_server(cfg, server):
     server.copy_file_to_server(cfg)
+
+
+def _get_board_boot_logs(timeout: int) -> str:
+    """Gets the console log for the boot process.
+
+    :param timeout: time value to collect the logs for
+    :type timeout: int
+    :return: Console Logs
+    :rtype: str
+    """
+    board = get_device_by_name("board")
+    return board.get_board_logs(timeout)
+
+
+def _verify_cm_config_downloaded(boot_logs: str) -> bool:
+    """Verify if the CM config download is successful
+
+    :param boot_logs: The console logs collected post reboot
+    :type boot_logs: str
+    :return: True if CM config download is successful else false
+    :rtype: bool
+    """
+    board = get_device_by_name("board")
+    return all(
+        [
+            message in boot_logs
+            for message in board.provisioning_messages["verify_cm_cfg_file_download"]
+        ]
+    )
+
+
+def _verify_emta_config_downloaded(boot_logs: str) -> bool:
+    """Verify if the MTA config download is successful
+
+    :param boot_logs: The console logs collected post reboot
+    :type boot_logs: str
+    :return: True if MTA config download is successful else false
+    :rtype: bool
+    """
+    board = get_device_by_name("board")
+    return board.provisioning_messages["verify_emta_cfg_file_download"] in boot_logs
+
+
+def _verify_emta_config_applied(boot_logs: str) -> bool:
+    """Verify if the MTA config is applied successfully
+
+    :param boot_logs: The console logs collected post reboot
+    :type boot_logs: str
+    :return: True if MTA config is applied successfully else false
+    :rtype: bool
+    """
+    board = get_device_by_name("board")
+    return board.provisioning_messages["verify_emta_config_apply"] in boot_logs
+
+
+def _verify_emta_provisioning(boot_logs: str) -> bool:
+    """Verify if the MTA provisioning is successful
+
+    :param boot_logs: The console logs collected post reboot
+    :type boot_logs: str
+    :return: True if MTA provisioning is successful else false
+    :rtype: bool
+    """
+    board = get_device_by_name("board")
+    return board.provisioning_messages["verify_emta_provisioning"] in boot_logs
+
+
+def verify_boot_stages_and_provisioning(timeout: int) -> bool:
+    """Collect the boot logs and validate the boot stages and provisioning
+
+    :param timeout: time value to collect the logs for
+    :type timeout: int
+    :return: True if boot stages are verified and provisioning is successful else false
+    :rtype: bool
+    """
+    board = get_device_by_name("board")
+    try:
+        board.hw.wait_for_hw_boot()
+    except Exception:
+        raise CodeError("Board Reboot not performed")
+    log = _get_board_boot_logs(timeout)
+    return all(
+        [
+            _verify_cm_config_downloaded(log),
+            _verify_emta_config_downloaded(log),
+            _verify_emta_config_applied(log),
+            _verify_emta_provisioning(log),
+        ]
+    )
