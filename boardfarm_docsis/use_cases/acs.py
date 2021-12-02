3d1a7ff86601b7338f246428e38fadf590921d19
Ketan Tewari <ktewari@libertyglobal.com>
2021-12-02 15:48:30 +0100

    feat(acs): add spv and gpv use cases
    Signed-off-by: Ketan Tewari <ktewari@libertyglobal.com>
    Change-Id: I27d795ddbd5ec95f5abfec0238b47e9ff7bc9233
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm-docsis/+/14210
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Oleksii Popov <opopov.contractor@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm_docsis/use_cases/acs.py
+++ boardfarm_docsis/use_cases/acs.py
@@ -1,4 +1,6 @@
+"""ACS Use cases."""
 import logging
+from typing import Any, Dict, List, TypedDict, Union
 
 import pexpect
 from boardfarm.exceptions import PexpectErrorTimeout, TR069FaultCode
@@ -9,15 +11,25 @@ from termcolor import colored
 logger = logging.getLogger("bft")
 
 
+class RPCOutput(TypedDict):
+    """Output of TR069 RPC operation on a paramter.
+
+    Resultant is a dictionary with keys specified as attribute of this class.
+    """
+
+    key: str
+    type: str
+    value: Any
+
+
 def is_dut_online_on_acs() -> bool:
-    """Checks if the DUT is online on acs.
+    """To check if the DUT is online on acs.
 
     :return: True if devices is registered with acs and GPV
-    is successful for Device.DeviceInfo.SoftwareVersion else false
+        is successful for Device.DeviceInfo.SoftwareVersion else false
     :rtype: bool
     """
     board = get_device_by_name("board")
-    acs_server = get_device_by_name("acs_server")
     try:
         acs_status = retry(board.tr069_connected, 3, [False])
     except PexpectErrorTimeout:
@@ -35,8 +47,44 @@ def is_dut_online_on_acs() -> bool:
     for _ in range(2):
         board.expect(pexpect.TIMEOUT, timeout=60)
         try:
-            return bool(acs_server.GPV("Device.DeviceInfo.SoftwareVersion"))
+            return bool(GPV("Device.DeviceInfo.SoftwareVersion"))
         except TR069FaultCode as err:
             msg = f"\n\nFailed to get DeviceInfo.SoftwareVersion due to {err} retrying"
             logger.error(colored(msg, color="red", attrs=["bold"]))
     return False
+
+
+def GPV(params: Union[str, List[str]]) -> List[RPCOutput]:
+    """Perform TR069 RPC call GetParameterValues.
+
+    Usage:
+    ..code-block:: python
+
+        GPV(params=["param1", "param2"])
+
+    :param params: List of parameters
+    :type params: Union[str, List[str]]
+    :raises TR069FaultCode: incase GPV operation fails
+    :return: List of dict of Param,Value pairs
+    :rtype: List[RPCOutput]
+    """
+    acs_server = get_device_by_name("acs_server")
+    return acs_server.GPV(params)
+
+
+def SPV(params: List[Dict[str, Any]]) -> List[RPCOutput]:
+    """Perform TR069 RPC call SetParameterValues.
+
+    Usage:
+    ..code-block:: python
+
+        SPV(params=[{"param1":"value1"}, {"param2":123}])
+
+    :param params: Dict or list of Dict[parameters, values]
+    :type params: Union[Dict[str, Any], List[Dict[str, Any]]]
+    :raises TR069FaultCode: incase SPV operation fails
+    :return: List of dict of Param,Value pairs
+    :rtype: List[RPCOutput]
+    """
+    acs_server = get_device_by_name("acs_server")
+    return acs_server.SPV(params)
