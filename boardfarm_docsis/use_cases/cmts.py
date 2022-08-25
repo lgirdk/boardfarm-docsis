f1bef2a34540ba31362d66804249084b25372fea
Filippo Carra <fcarra@libertyglobal.com>
2022-08-25 12:20:05 +0200

    feat(use_cases/cmts): allow CMTS to be passed as parameter for is_route_present_on_cmts()
    latest design principles indicate to pass the devices to the Use Cases
    
    Signed-off-by: Filippo Carra <fcarra@libertyglobal.com>
    Change-Id: I766286ef0ecf8e66f62619362242874f487e6386
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm-docsis/+/16721
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm_docsis/use_cases/cmts.py
+++ boardfarm_docsis/use_cases/cmts.py
@@ -2,7 +2,7 @@
 import logging
 from ipaddress import IPv4Network, IPv6Network, ip_network
 from re import findall as regex_findall
-from typing import Union
+from typing import Optional, Union
 
 from boardfarm.lib.DeviceManager import get_device_by_name
 from boardfarm.lib.regexlib import ValidIpv4AddressRegex_Nogroup
@@ -12,7 +12,9 @@ from boardfarm_docsis.devices.base_devices.cmts_template import CmtsTemplate
 logger = logging.getLogger("bft")
 
 
-def is_route_present_on_cmts(route: Union[IPv4Network, IPv6Network]) -> bool:
+def is_route_present_on_cmts(
+    route: Union[IPv4Network, IPv6Network], which_cmts: Optional[CmtsTemplate] = None
+) -> bool:
     """Check if routing table of cmts router contains a route.
 
     perfrom ip route command on quagga router, collet the routes and
@@ -27,11 +29,18 @@ def is_route_present_on_cmts(route: Union[IPv4Network, IPv6Network]) -> bool:
 
     :param route: route to be looked up on CMTS
     :type route: Union[IPv4Network, IPv6Network]
+    :param which_cmts: CMTS to be used, defaults to None.
+        If None is given, it will be automatically selected.
+    :type which_cmts: Optional[CmtsTemplate], optional
     :return: True if route is present on CMTS
     :rtype: bool
     """
 
-    cmts: CmtsTemplate = get_device_by_name("cmts")
+    if which_cmts is None:
+        cmts: CmtsTemplate = get_device_by_name("cmts")
+    else:
+        cmts = which_cmts
+
     raw_routes = cmts.ip_route()
     ip_routes = [
         ip_network(_route)
