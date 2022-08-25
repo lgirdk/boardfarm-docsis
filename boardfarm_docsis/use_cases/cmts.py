29bfb37eba012fd583f82b8b43d2ac5467363f5a
Filippo Carra <fcarra@libertyglobal.com>
2022-08-25 12:09:25 +0200

    refactor(use_cases/cmts): Fix type hinting
    Signed-off-by: Filippo Carra <fcarra@libertyglobal.com>
    Change-Id: I8fe051b2181fe6f0ebfb2c9cf8088e23caaeac62
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm-docsis/+/16720
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm_docsis/use_cases/cmts.py
+++ boardfarm_docsis/use_cases/cmts.py
@@ -1,33 +1,40 @@
-"""mini cmts use cases library."""
-import ipaddress
+"""Use Cases to interact with DOCSIS devices such as CMTS and CM."""
 import logging
+from ipaddress import IPv4Network, IPv6Network, ip_network
 from re import findall as regex_findall
+from typing import Union
 
 from boardfarm.lib.DeviceManager import get_device_by_name
 from boardfarm.lib.regexlib import ValidIpv4AddressRegex_Nogroup
 
+from boardfarm_docsis.devices.base_devices.cmts_template import CmtsTemplate
+
 logger = logging.getLogger("bft")
 
 
-def is_route_present_on_cmts(route: ipaddress.ip_network) -> bool:
+def is_route_present_on_cmts(route: Union[IPv4Network, IPv6Network]) -> bool:
     """Check if routing table of cmts router contains a route.
 
     perfrom ip route command on quagga router, collet the routes and
     check if route is present in table output.
-    .. highlight:: python
-    ..code-block:: python
 
+    .. code-block:: python
+
+        # example usage
         status = is_route_present_on_cmts(
             route=ipaddress.ip_network('192.168.101.0/24')
-            )
+        )
 
-    :return: True if route is present on cmts else false
+    :param route: route to be looked up on CMTS
+    :type route: Union[IPv4Network, IPv6Network]
+    :return: True if route is present on CMTS
     :rtype: bool
     """
-    cmts = get_device_by_name("cmts")
+
+    cmts: CmtsTemplate = get_device_by_name("cmts")
     raw_routes = cmts.ip_route()
     ip_routes = [
-        ipaddress.ip_network(_route)
+        ip_network(_route)
         for _route in regex_findall(
             ValidIpv4AddressRegex_Nogroup + r"\/[\d]{1,2}", raw_routes
         )
