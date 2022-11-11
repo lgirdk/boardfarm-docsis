ec2bf8e7db739967d6c0f3805513b9d415e63d58
Michele Gualco <msalvatoregualco@libertyglobal.com>
2022-11-11 07:08:17 +0100

    feat: add docsis mta compilation
    Signed-off-by: Michele Gualco <msalvatoregualco@libertyglobal.com>
    Change-Id: Id07cd10a486803b4b806393da187ae728dd98e6e
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm-docsis/+/17904
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Musthafa Kariyaparambil <mkariyaparambil.contractor@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm3_docsis/lib/docsis_encoder.py
+++ boardfarm3_docsis/lib/docsis_encoder.py
@@ -13,37 +13,84 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class DocsisConfigEncoder:
-    """DOCSIS config file encoder."""
+    """DOCSIS config file encoder.
 
-    # pylint: disable=too-few-public-methods
+    Requires the docsis compiler to be installed locally.
+    """
 
     def __init__(self) -> None:
         """Initialize docsis config encoder."""
         self._encoder_cmd = "docsis"
+        self._cfg_dict = {
+            "cm": {
+                "encoded_ext": ".cfg",
+                "option": "-e",
+                "key_file": "/dev/null",  # To be considered: parameterise
+            },
+            "mta": {
+                "encoded_ext": ".bin",
+                "option": "-p",
+                "key_file": "",  # To be considered: parameterise
+            },
+        }
 
-    def encode_cm_config(self, cm_config: str, mibs_path: list[str]) -> Path:
-        """Encode cable modem config text file(boot file).
-
-        :param cm_config: cable modem config text file
-        :param mibs_path: mibs directory paths
-        :raises ConfigEncodingError: when docsis encoding failed
-        :returns: path to docsis encoded config file
-        """
-        print(cm_config)
+    def _encode_config(self, config: str, mibs_path: list[str], is_mta: bool) -> Path:
         mibs_argument = ":".join(mibs_path)
+        prefix = "mta" if is_mta else "cm"
         with tempfile.NamedTemporaryFile(
-            mode="w", prefix="cm-config-", suffix=".txt", encoding="utf-8"
+            mode="w",
+            prefix=f"{prefix}-config-",
+            suffix=".txt",
+            encoding="utf-8",
         ) as named_temp_file:
-            named_temp_file.write(cm_config)
+            named_temp_file.write(config)
             named_temp_file.flush()
-            cfg_file_path = Path(named_temp_file.name.replace(".txt", ".cfg"))
+            cfg_file_path = Path(
+                named_temp_file.name.replace(
+                    ".txt",
+                    self._cfg_dict[prefix]["encoded_ext"],
+                )
+            )
             command = (
                 f"{self._encoder_cmd} -M {mibs_argument} "
-                f"-e {named_temp_file.name} /dev/null {cfg_file_path}"
+                f"{self._cfg_dict[prefix]['option']} {named_temp_file.name} "
+                f"{self._cfg_dict[prefix]['key_file']} {cfg_file_path}"
             )
-            _LOGGER.debug("Encoding cable modem config: %s", command)
+            _LOGGER.debug("Encoding modem config: %s", command)
             session = LocalCmd("docsis-encoder", command, [])
             session.expect(pexpect.EOF)
             if session.wait() != 0:
-                raise ConfigEncodingError("Failed to encode cable modem config")
+                raise ConfigEncodingError(
+                    f"Failed to encode modem config {named_temp_file.name}"
+                )
         return cfg_file_path
+
+    def encode_cm_config(self, cm_config: str, mibs_path: list[str]) -> Path:
+        """Encode a given cable modem config text file(boot file).
+
+        Override this method for bespoke compilations (e.g, different encriptions)
+
+        :param cm_config: cable modem config text file path
+        :type cm_config: str
+        :param mibs_path: mibs directory paths
+        :type mibs_path: list[str]
+        :raises: ConfigEncodingError when docsis encoding failed
+        :return: path to the docsis encoded config file
+        :rtype: Path
+        """
+        return self._encode_config(config=cm_config, mibs_path=mibs_path, is_mta=False)
+
+    def encode_mta_config(self, mta_config: str, mibs_path: list[str]) -> Path:
+        """Encode a given MTA config text file.
+
+        Override this method for bespoke compilations (e.g, different encriptions)
+
+        :param mta_config: MTA config text file path
+        :type mta_config: str
+        :param mibs_path: mibs directory paths
+        :type mibs_path: list[str]
+        :raises: ConfigEncodingError when docsis encoding failed
+        :return: path to the docsis encoded MTA file
+        :type: Path
+        """
+        return self._encode_config(config=mta_config, mibs_path=mibs_path, is_mta=True)
