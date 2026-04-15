e78f28cad6eb9b23a9bdbab1fae2975c0a5cac66
Saurav Kochar <skochar.contractor@libertyglobal.com>
2026-04-15 12:23:39 +0200

    feat(docsis_ntu.py): define templates for docsis ntu device class
    BOARDFARM-6250
    
    Change-Id: I03b46842bd1554d99eeb3adb5c4b617a6b6904d7
    Signed-off-by: Saurav Kochar <skochar.contractor@libertyglobal.com>
    Reviewed-on: http://gerrit.cicd.boardfarm.upclabs.com/c/github/boardfarm-docsis/+/4781
    Tested-by: Jenkins Automation
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

---
+++ boardfarm3_docsis/templates/ntu/docsis_ntu.py
@@ -0,0 +1,14 @@
+"""DOCSIS NTU template."""
+
+from __future__ import annotations
+
+from boardfarm3.templates.ntu import NTU
+
+
+class DocsisNTU(NTU):
+    """DOCSIS NTU Template.
+
+    This will be defined in the future when we have a better
+    understanding of the commonalities between the various NTUs
+    used in the DOCSIS tests.
+    """
