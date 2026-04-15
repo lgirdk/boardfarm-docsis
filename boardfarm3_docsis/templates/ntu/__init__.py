831cd54191b1e9b8e9e46dcd9d50cb3593dd6fd4
Saurav Kochar <skochar.contractor@libertyglobal.com>
2026-04-15 12:23:39 +0200

    feat(docsis_ntu.py): define templates for docsis ntu device class
    BOARDFARM-6250
    
    Change-Id: I03b46842bd1554d99eeb3adb5c4b617a6b6904d7
    Signed-off-by: Saurav Kochar <skochar.contractor@libertyglobal.com>
    Reviewed-on: http://gerrit.cicd.boardfarm.upclabs.com/c/github/boardfarm-docsis/+/4781
    Tested-by: Jenkins Automation
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Reviewed-on: http://gerrit.cicd.boardfarm.upclabs.com/c/github/boardfarm-docsis/+/4893
    Tested-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

---
+++ boardfarm3_docsis/templates/ntu/__init__.py
@@ -0,0 +1,7 @@
+"""Boardfarm LGI shared cable modem templates."""
+
+from boardfarm3.templates.ntu import NTU
+
+from boardfarm3_docsis.templates.ntu.docsis_ntu import DocsisNTU
+
+__all__ = ["NTU", "DocsisNTU"]
