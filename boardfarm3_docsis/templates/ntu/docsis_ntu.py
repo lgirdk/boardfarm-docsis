b7e2d12c09d778d42e3e8b7da99adfb8a628eacd
Sanjai Kannan <sanjai.kannan@infosys.com>
2026-06-22 16:45:10 +0530

    fix(docsis_ntu.py): add template methods
    BOARDFARM-6618
    
    Change-Id: Ic4fd6946fbb98c2ebf9a00f6d4e074036051b42d
    Signed-off-by: Sanjai Kannan <sanjai.kannan@infosys.com>
    Reviewed-on: http://gerrit.cicd.boardfarm.upclabs.com/c/github/boardfarm-docsis/+/5507
    Tested-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm3_docsis/templates/ntu/docsis_ntu.py
+++ boardfarm3_docsis/templates/ntu/docsis_ntu.py
@@ -2,7 +2,9 @@
 
 from __future__ import annotations
 
-from boardfarm3.templates.ntu import NTU
+from abc import abstractmethod
+
+from boardfarm3.templates.ntu.ntu import NTU
 
 
 class DocsisNTU(NTU):
@@ -12,3 +14,39 @@ class DocsisNTU(NTU):
     understanding of the commonalities between the various NTUs
     used in the DOCSIS tests.
     """
+
+    @property
+    @abstractmethod
+    def cm_mac(self) -> str:
+        """MAC Address.
+
+        :return: the CM mac address (usually the wan0 mac address)
+        :rtype: str
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_online(self) -> bool:
+        """Is cable modem online.
+
+        :return: True if cable modem is in OPERATIONAL state, False otherwise
+        :rtype: bool
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def finalize_boot(self) -> bool:
+        """Check if board SKU is the same as env & apply correct SKU if it is not set.
+
+        .. note::
+            * See OFW-1935 for details. Steps below
+            * Send dmcli GET Device.X_LGI-COM_General.CustomerId
+            * If it is the same as requested in env.json -> return True
+            * echo {SKU_index} > /nvram/bootconfig_custindex
+            * sync DUT FS
+            * Reboot DUT
+
+        :return: True if SKU already set correctly, False if we set it & reboot board
+        :rtype: bool
+        """
+        raise NotImplementedError
