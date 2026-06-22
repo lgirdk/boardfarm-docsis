43129502d8fe25e5125685dec37ad5c043f9bcce
Sanjai Kannan <sanjai.kannan@infosys.com>
2026-06-22 16:45:10 +0530

    fix(docsis_ntu.py): add template methods
    BOARDFARM-6618
    
    Change-Id: Ic4fd6946fbb98c2ebf9a00f6d4e074036051b42d
    Signed-off-by: Sanjai Kannan <sanjai.kannan@infosys.com>
    Reviewed-on: http://gerrit.cicd.boardfarm.upclabs.com/c/github/boardfarm-docsis/+/5509
    Tested-by: Jenkins Automation
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm3_docsis/templates/ntu/__init__.py
+++ boardfarm3_docsis/templates/ntu/__init__.py
@@ -1,6 +1,6 @@
 """Boardfarm LGI shared cable modem templates."""
 
-from boardfarm3.templates.ntu import NTU
+from boardfarm3.templates.ntu.ntu import NTU
 
 from boardfarm3_docsis.templates.ntu.docsis_ntu import DocsisNTU
 
